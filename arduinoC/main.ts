90d0618bc6d80a6cb0cf89daf22582b10fc7e4ad
YFROBOTZL <yfrobot-zl@foxmail.com>
2022-11-14 17:32:23 +0800

    update

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -138,22 +138,21 @@ namespace valon {
         Generator.addInclude(`definevalonmotor`, `PROGMEM void motorDrive(int mot, int speed, int dir); // valon motor 控制函数`)
         Generator.addInclude(`definevalonmotorFun`, `// valon motor 控制函数\n`+
             `void motorDrive(int mot, int speed, int dir) {\n`+
-            `  int sp = map(speed, 0, 255, 0, 1023);\n`+
             `  if (mot == 0) {    // 右电机\n`+
             `    if (dir == 0) {  // 正转\n`+
-            `      digitalWrite(P16, LOW);\n`+
-            `      analogWrite(P15, sp);\n`+
+            `      digitalWrite(9, LOW);\n`+
+            `      analogWrite(6, speed);\n`+
             `    } else {         // 反转\n`+
-            `      digitalWrite(P16, HIGH);\n`+
-            `      analogWrite(P15, sp);\n`+
+            `      digitalWrite(9, HIGH);\n`+
+            `      analogWrite(6, speed);\n`+
             `    }\n`+
             `  } else {           // 左电机\n`+
             `    if (dir == 0) {  // 正转\n`+
-            `      digitalWrite(P13, LOW);\n`+
-            `      analogWrite(P14, sp);\n`+
+            `      digitalWrite(4, LOW);\n`+
+            `      analogWrite(5, speed);\n`+
             `    } else {         // 反转\n`+
-            `      digitalWrite(P13, HIGH);\n`+
-            `      analogWrite(P14, sp);\n`+
+            `      digitalWrite(4, HIGH);\n`+
+            `      analogWrite(5, speed);\n`+
             `    }\n`+
             `  }\n`+
             `}`
@@ -186,22 +185,21 @@ namespace valon {
         Generator.addInclude(`definevalonmotor`, `PROGMEM void motorDrive(int mot, int speed, int dir); // valon motor 控制函数`)
         Generator.addInclude(`definevalonmotorFun`, `// valon motor 控制函数\n`+
             `void motorDrive(int mot, int speed, int dir) {\n`+
-            `  int sp = map(speed, 0, 255, 0, 1023);\n`+
             `  if (mot == 0) {    // 右电机\n`+
             `    if (dir == 0) {  // 正转\n`+
-            `      digitalWrite(P16, LOW);\n`+
-            `      analogWrite(P15, sp);\n`+
+            `      digitalWrite(9, LOW);\n`+
+            `      analogWrite(6, speed);\n`+
             `    } else {         // 反转\n`+
-            `      digitalWrite(P16, HIGH);\n`+
-            `      analogWrite(P15, sp);\n`+
+            `      digitalWrite(9, HIGH);\n`+
+            `      analogWrite(6, speed);\n`+
             `    }\n`+
             `  } else {           // 左电机\n`+
             `    if (dir == 0) {  // 正转\n`+
-            `      digitalWrite(P13, LOW);\n`+
-            `      analogWrite(P14, sp);\n`+
+            `      digitalWrite(4, LOW);\n`+
+            `      analogWrite(5, speed);\n`+
             `    } else {         // 反转\n`+
-            `      digitalWrite(P13, HIGH);\n`+
-            `      analogWrite(P14, sp);\n`+
+            `      digitalWrite(4, HIGH);\n`+
+            `      analogWrite(5, speed);\n`+
             `    }\n`+
             `  }\n`+
             `}`
@@ -222,22 +220,21 @@ namespace valon {
         Generator.addInclude(`definevalonmotor`, `PROGMEM void motorDrive(int mot, int speed, int dir); // valon motor 控制函数`)
         Generator.addInclude(`definevalonmotorFun`, `// valon motor 控制函数\n`+
             `void motorDrive(int mot, int speed, int dir) {\n`+
-            `  int sp = map(speed, 0, 255, 0, 1023);\n`+
             `  if (mot == 0) {    // 右电机\n`+
             `    if (dir == 0) {  // 正转\n`+
-            `      digitalWrite(P16, LOW);\n`+
-            `      analogWrite(P15, sp);\n`+
+            `      digitalWrite(9, LOW);\n`+
+            `      analogWrite(6, speed);\n`+
             `    } else {         // 反转\n`+
-            `      digitalWrite(P16, HIGH);\n`+
-            `      analogWrite(P15, sp);\n`+
+            `      digitalWrite(9, HIGH);\n`+
+            `      analogWrite(6, speed);\n`+
             `    }\n`+
             `  } else {           // 左电机\n`+
             `    if (dir == 0) {  // 正转\n`+
-            `      digitalWrite(P13, LOW);\n`+
-            `      analogWrite(P14, sp);\n`+
+            `      digitalWrite(4, LOW);\n`+
+            `      analogWrite(5, speed);\n`+
             `    } else {         // 反转\n`+
-            `      digitalWrite(P13, HIGH);\n`+
-            `      analogWrite(P14, sp);\n`+
+            `      digitalWrite(4, HIGH);\n`+
+            `      analogWrite(5, speed);\n`+
             `    }\n`+
             `  }\n`+
             `}`
@@ -253,22 +250,21 @@ namespace valon {
         Generator.addInclude(`definevalonmotor`, `PROGMEM void motorDrive(int mot, int speed, int dir); // valon motor 控制函数`)
         Generator.addInclude(`definevalonmotorFun`, `// valon motor 控制函数\n`+
             `void motorDrive(int mot, int speed, int dir) {\n`+
-            `  int sp = map(speed, 0, 255, 0, 1023);\n`+
             `  if (mot == 0) {    // 右电机\n`+
             `    if (dir == 0) {  // 正转\n`+
-            `      digitalWrite(P16, LOW);\n`+
-            `      analogWrite(P15, sp);\n`+
+            `      digitalWrite(9, LOW);\n`+
+            `      analogWrite(6, speed);\n`+
             `    } else {         // 反转\n`+
-            `      digitalWrite(P16, HIGH);\n`+
-            `      analogWrite(P15, sp);\n`+
+            `      digitalWrite(9, HIGH);\n`+
+            `      analogWrite(6, speed);\n`+
             `    }\n`+
             `  } else {           // 左电机\n`+
             `    if (dir == 0) {  // 正转\n`+
-            `      digitalWrite(P13, LOW);\n`+
-            `      analogWrite(P14, sp);\n`+
+            `      digitalWrite(4, LOW);\n`+
+            `      analogWrite(5, speed);\n`+
             `    } else {         // 反转\n`+
-            `      digitalWrite(P13, HIGH);\n`+
-            `      analogWrite(P14, sp);\n`+
+            `      digitalWrite(4, HIGH);\n`+
+            `      analogWrite(5, speed);\n`+
             `    }\n`+
             `  }\n`+
             `}`
@@ -322,6 +318,84 @@ namespace valon {
         Generator.addInclude("include_DFRobot_URM10", `#include <DFRobot_URM10.h>`);
         Generator.addObject("object_DFRobot_URM10_valon", `DFRobot_URM10`, `valon_sr04;`);
         Generator.addCode(`valon_sr04.getDistanceCM(P8,P9)`);
+    }
+
+    
+    let valonoTone = `ValonTone`;
+    export enum VALONBEAT {
+        //% block="1/2"
+        500,
+        //% block="1/4"
+        250,
+        //% block="1/8"
+        125,
+        //% block="整拍"
+        1000,
+        //% block="双拍"
+        2000,
+        //% block="停止"
+        0,
+    }
+    
+    //% block="蜂鸣器音调为[TONE]节拍为[BEAT]" blockType="command"
+    //% TONE.shadow="note" TONE.defl=247
+    //% BEAT.shadow="dropdown" BEAT.options="valon.VALONBEAT"" BEAT.defl="valon.VALONBEAT.500"
+    export function buzzerTone(parameter: any, block: any) {
+        let tone = parameter.TONE.code;
+        let beat = parameter.BEAT.code;
+        Generator.addInclude(`Include_DFRobot_Libraries`, `#include <DFRobot_Libraries.h>`)
+        Generator.addObject(`DFRobot_Tone`, `DFRobot_Tone`, `${valonoTone};`);
+        Generator.addCode(`${valonoTone}.play(11, ${tone}, ${beat});`);
+    }
+
+    //% block="---"
+    export function noteSep() { }
+    //% block="---"
+    export function noteSep2() { }
+
+
+
+    let valonoled = `valon_oled`;
+
+    //% block="OLED 初始化" blockType="command"
+    export function oledInit(parameter: any, block: any) {
+        Generator.addInclude(`Include_DFRobot_SSD1306_I2C`, `#include <DFRobot_SSD1306_I2C.h>`)
+        Generator.addObject(`DFRobot_SSD1306_I2C`, `DFRobot_SSD1306_I2C`, `${valonoled};`);
+        Generator.addSetup(`${valonoled}.begin`, `${valonoled}.begin(0x3d);`);
+    }
+
+    //% block="OLED 第[LINE]行 显示[STRING]" blockType="command"
+    //% LINE.shadow="range"   LINE.params.min=1    LINE.params.max=4    LINE.defl=1
+    //% STRING.shadow="string"   STRING.defl="valon"
+    export function oledShowLine(parameter: any, block: any) {
+        let line = parameter.LINE.code;
+        let string = parameter.STRING.code;
+        Generator.addInclude(`Include_DFRobot_SSD1306_I2C`, `#include <DFRobot_SSD1306_I2C.h>`)
+        Generator.addObject(`DFRobot_SSD1306_I2C`, `DFRobot_SSD1306_I2C`, `${valonoled};`);
+        Generator.addSetup(`${valonoled}.begin`, `${valonoled}.begin(0x3d);`);
+        Generator.addCode(`${valonoled}.setCursorLine(${line});\n  ${valonoled}.printLine(${string});`);
+    }
+
+    //% block="OLED 在坐标X:[X]Y:16*[Y]显示[STRING]" blockType="command"
+    //% X.shadow="range"   X.params.min=0    X.params.max=127    X.defl=0
+    //% Y.shadow="range"   Y.params.min=1    Y.params.max=4    Y.defl=1
+    //% STRING.shadow="string"   STRING.defl="valon"
+    export function oledShowXY(parameter: any, block: any) {
+        let x = parameter.X.code;
+        let y = parameter.Y.code;
+        let string = parameter.STRING.code;
+        Generator.addInclude(`Include_DFRobot_SSD1306_I2C`, `#include <DFRobot_SSD1306_I2C.h>`)
+        Generator.addObject(`DFRobot_SSD1306_I2C`, `DFRobot_SSD1306_I2C`, `${valonoled};`);
+        Generator.addSetup(`${valonoled}.begin`, `${valonoled}.begin(0x3d);`);
+        Generator.addCode(`${valonoled}.setCursor(${x},${y}-1);\n  ${valonoled}.print(${string});`);
+    }
+    
+    //% block="OLED 清屏" blockType="command"
+    export function oledClean(parameter: any, block: any) {
+        Generator.addInclude(`Include_DFRobot_SSD1306_I2C`, `#include <DFRobot_SSD1306_I2C.h>`)
+        Generator.addObject(`DFRobot_SSD1306_I2C`, `DFRobot_SSD1306_I2C`, `${valonoled};`);
+        Generator.addSetup(`${valonoled}.begin`, `${valonoled}.begin(0x3d);`);
+        Generator.addCode(`${valonoled}.fillScreen(0);`);
     }
 
 }
